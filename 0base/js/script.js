6eb14058e21b8220de9b0bc0b12aeec053c47c68
Your Name <you@example.com>
2021-06-22 00:41:22 +0700

    .

--- 0base/js/script.js
+++ 0base/js/script.js
@@ -2,5 +2,23 @@ $(function () {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $(".red-box").fadeOut(2000);
+  // jQuery(".red-box").fadeOut(2000);
+
+  // $(".red-box").fadeOut(2000);
+  // $(".red-box").fadeIn(3000);
+
+  // $(".red-box").fadeTo(500, 0.5);
+  // $(".red-box").fadeTo(500, 0.9);
+  // $(".red-box").fadeTo(500, 0.1);
+  // $(".red-box").fadeTo(500, 0.8);
+  // $(".red-box").fadeTo(500, 0.2);
+  // $(".red-box").fadeTo(500, 0.7);
+
+  // $(".blue-box").fadeToggle(500);
+
+  // $(".blue-box").show();
+  // $(".blue-box").hide();
+  // $(".blue-box").toggle();
+
+
 });
